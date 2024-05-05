9470f11f8eb3ee742ca76466880d16c7707276fb
aplicalvin <thecalvins.sims@gmail.com>
2024-05-05 14:20:27 +0700

    Add slider in DisplayTasHome - Add slider from swiperjs.com - Done adding animate to displaytas,
    What to do next?
    - Manipulate image inside the slider (the bag design)
    - Sinkronisasi dengan DisplayBottleHome untuk jumlah botol per bag nya.
    
    by ApliCalvin on May'5 2024 at 14.20

--- src/components/app/Home/DisplayTasHome.jsx
+++ src/components/app/Home/DisplayTasHome.jsx
@@ -1,9 +1,65 @@
 // import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-flip";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+// import required modules
+import { EffectFlip } from "swiper/modules";
 
 function DisplayTasHome() {
     return (
         <>
-            <div>DisplayTasHome</div>
+            <div>
+                <Swiper
+                    effect={"flip"}
+                    grabCursor={true}
+                    // pagination={true}
+                    // navigation={true}
+                    modules={[EffectFlip]}
+                    className="mySwiper max-w-xs"
+                >
+                    <SwiperSlide>
+                        <img
+                            src="https://swiperjs.com/demos/images/nature-1.jpg"
+                            className="rounded-3xl"
+                        />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img
+                            src="https://swiperjs.com/demos/images/nature-2.jpg"
+                            className="rounded-3xl"
+                        />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img
+                            src="https://swiperjs.com/demos/images/nature-3.jpg"
+                            className="rounded-3xl"
+                        />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img
+                            src="https://swiperjs.com/demos/images/nature-4.jpg"
+                            className="rounded-3xl"
+                        />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img
+                            src="https://swiperjs.com/demos/images/nature-5.jpg"
+                            className="rounded-3xl"
+                        />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img
+                            src="https://swiperjs.com/demos/images/nature-6.jpg"
+                            className="rounded-3xl"
+                        />
+                    </SwiperSlide>
+                </Swiper>
+            </div>
         </>
     );
 }
