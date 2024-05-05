c978d058ec9b22cf41f5f2a177c951c49b7a6362
aplicalvin <thecalvins.sims@gmail.com>
2024-05-05 12:57:52 +0700

    style: add Home/DisplayBotol - Add displayBotol with progress bar from hyperui.dev - Add progressWidth variable, type=string, the prosentase of bottle
    by ApliCalvin on May'5 2024 at 12.57

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -6,7 +6,7 @@ const AppLayout = () => {
         <>
             <div className="bg-primary-900 w-screen h-screen font-dmsans">
                 <div className="grid bg-white border  max-w-sm mx-auto min-h-screen content-between">
-                    <div className="px-6 py-4 border border-black">
+                    <div className="px-3 py-4 border border-black">
                         <Outlet />
                     </div>
                     <BottomNav />
