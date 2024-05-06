25e046c3d12b61867a760cce4ee9318b1a6a6c27
aplicalvin <thecalvins.sims@gmail.com>
2024-05-06 17:27:10 +0700

    style: Add TopBar to All App Page
    - Move TopBarHome.jsx to ./src/components/app/ and change name to TopBar.jsx
    - Change the function too, from TopBarHome to TopBar
    - Change AppLayout structure, adding TopBar.
    
    by ApliCalvin on May'6 2024 at 17.27

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -1,13 +1,17 @@
 import { Outlet } from "react-router-dom";
 import BottomNav from "../components/app/BottomNav";
+import TopBar from "../components/app/TopBar";
 
 const AppLayout = () => {
     return (
         <>
             <div className="bg-primary-900 w-screen h-screen font-dmsans">
                 <div className="grid bg-white border max-w-sm mx-auto h-screen content-between">
-                    <div className="px-3 py-4 border overflow-x-auto">
-                        <Outlet />
+                    <div>
+                        <TopBar />
+                        <div className="px-3 py-4 border overflow-x-auto">
+                            <Outlet />
+                        </div>
                     </div>
                     <BottomNav />
                 </div>
--- src/pages/app/Home.jsx
+++ src/pages/app/Home.jsx
@@ -1,13 +1,11 @@
 import DisplayTasHome from "../../components/app/Home/DisplayTasHome";
 import NearModHome from "../../components/app/Home/NearModHome";
-import TopBarHome from "../../components/app/Home/TopBarHome";
 
 // import React from "react";
 function Home() {
     return (
         <>
             <div className="grid gap-3">
-                <TopBarHome />
                 <DisplayTasHome />
                 <NearModHome />
             </div>
