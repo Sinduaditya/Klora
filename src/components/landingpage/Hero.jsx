7c777d6e8c8c66fa63b050aa2cc43672f1f92639
aplicalvin <thecalvins.sims@gmail.com>
2024-05-07 09:51:14 +0700

    fixed landing by apcn may'7 24 09.51

--- src/components/landingpage/Hero.jsx
+++ src/components/landingpage/Hero.jsx
@@ -14,7 +14,7 @@ function Hero() {
     return (
         <div className="w-full text-text font-outfit md:pt-[1vh] mb-12">
             <div className="xl:max-w-5xl mx-auto grid lg:flex lg:flex-row lg:justify-between items-center  space-x-10 px-10 xl:px-0 flex-col-reverse w-fit">
-                <article className="flex flex-col space-y-8 text-center lg:text-left mx-auto">
+                <article className="flex order-2 md:order-1 flex-col space-y-8 text-center lg:text-left mx-auto">
                     <h1 className="text-5xl lg:text-7xl font-bold text-neutral-900 leading-[110%] tracking-tigh">
                         Tukar <i>botol</i> sekali pakai <br /> menjadi{" "}
                         <span className="bg-primary-200 underline italic">
@@ -38,7 +38,7 @@ function Hero() {
                     height={467}
                     data-aos="zoom-in-up"
                     data-aos-duration="1000"
-                    className="-mr-10 w-[250.5px] h-[230px] md:w-[495px] md:h-[467px]"
+                    className="-mr-10 w-[250.5px] h-[230px] md:w-[495px] md:h-[467px] md:order-2 "
                 />
             </div>
         </div>
